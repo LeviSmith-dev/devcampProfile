7ce1eee9cd1a4dbc76b4a254a865edb3a82dba70
Levi Smith <smith.levidev@gmail.com>
2017-04-14 16:23:55 -0600

    created update and create controller actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,13 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def show
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
@@ -13,7 +20,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolio_item, notice: 'Your Portfolio Item is now Live.' }
+        format.html { redirect_to portfoilios_path, notice: 'Your Portfolio Item is now Live.' }
         # format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
@@ -21,4 +28,27 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        # format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolio_path, notice: 'portfolio was successfully destroyed.' }
+      # format.json { head :no_content }
+    end
+  end
+
 end
